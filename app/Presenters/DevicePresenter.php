701751b01b58afcbb189a0230744fc14227c9aab
raunerj <raunerj@spseplzen.cz>
2024-01-05 09:39:10 +0100

    Základ API

--- app/Presenters/DevicePresenter.php
+++ app/Presenters/DevicePresenter.php
@@ -9,6 +9,16 @@ use Nette;
 
 final class DevicePresenter extends Nette\Application\UI\Presenter
 {
+    private $httprequest;
+
+    public function startup(): void
+    {
+	    parent::startup();
+	    /*if (!$this->getUser()->isLoggedIn()) {
+		    $this->redirect('Login:');
+	    }*/
+        $this->httprequest=$this->getHttpRequest();
+    }
 
     public function __construct(private Nette\Database\Explorer $database)
     {
@@ -20,7 +30,28 @@ final class DevicePresenter extends Nette\Application\UI\Presenter
         
         $this->template->actionName = $this->getPresenter()->action;
 
+        $result_kategorie = $this->database->query("SELECT * FROM kategorie WHERE id_uzivatele = ?", $this->user->getIdentity()->id);
+        $kat = [];
+        foreach($result_kategorie as $kategorie){
+            $results_tymy = $this->database->query("SELECT * FROM tymy WHERE id_kategorie = ? AND id_uzivatele = ? ORDER BY Tym ASC;", $kategorie->id, $this->user->getIdentity()->id);
+            $tymy = [];
+            foreach($results_tymy as $tym){
+                $tymy [$tym->id] = $tym->Tym;
+            }
+            $kat [$kategorie->kategorie] = $tymy;
+        }
+		$this->template->tymy = $kat;
+
     }
 
+    public function actionData($id) {
+
+        if($this->httprequest->isMethod('POST')){
+            $body=$this->httprequest->getRawBody();
+            $this->sendJson(json_encode($body));
+        }else{
+            $this->error();
+        }
+    }
 
 }
